f2dc46c5ad1e30e79c1dd7651e1b929d8952622c
Daniele Polencic <daniele.polencic@gmail.com>
2017-05-03 18:54:19 +0100

    fixed assets

--- index.js
+++ index.js
@@ -3,12 +3,10 @@ const app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
-app.use(express.static(__dirname + '/dist'));
+app.use('/static', express.static(`${__dirname}/src`));
 
-app.get('/', function(request, response) {
-  response.render('dist/index.html');
-});
+app.get('/', (request, response) => response.sendFile(`${__dirname}/src/index.html`));
 
-app.listen(app.get('port'), function() {
+app.listen(app.get('port'), () => {
   console.log('Node app is running on port', app.get('port'));
 });
